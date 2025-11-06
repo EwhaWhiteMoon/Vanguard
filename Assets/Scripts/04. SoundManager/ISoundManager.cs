8df53dd71b620dd7ae5d86fdc8298763ec8260e5
YunGuHan <hanyun9@naver.com>
2025-11-06 12:34:44 +0900

    SoundManager, EffectManager 인터페이스 구현

---
+++ Assets/Scripts/04. SoundManager/ISoundManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 사운드 재생 및 관리 기능을 제공하는 인터페이스
+/// </summary>
+
+public interface ISoundManager
+{
+    ///<summary>
+    ///배경음악 재생
+    /// </summary>
+    /// <param name = "ClipName"> 오디오 클립이름</param>
+    /// <param name="volume">볼륨 (0~1)</param>
+    /// <param name="loop">반복 여부</param>
+    void PlayBGM(string clipName, float volume = 1f, bool loop = true);
+    
+    
+    /// <summary>
+    /// 효과음(SFX) 재생.
+    /// </summary>
+    /// <param name="clipName">오디오 클립 이름</param>
+    /// <param name="volume">볼륨 (0~1)</param>
+    void PlaySFX(string ClipName, float volume = 1f);
+
+
+    /// <summary>
+    /// 현재 재생 중인 BGM 정지.
+    /// </summary>
+    void StopBGM();
+
+
+    /// <summary>
+    /// 전체 음량 제어.
+    /// </summary>
+    void SetMasterVolume(float volume);
+
+
+    /// <summary>
+    /// BGM 볼륨 조정.
+    /// </summary>
+    void SetBGMVolume(float volume);
+
+
+    /// <summary>
+    /// SFX 볼륨 조정.
+    /// </summary>
+    void SetSFXVolume(float volume);
+
+
+    /// <summary>
+    /// 오디오 클립 미리 등록.
+    /// </summary>
+    /// <param name="clip">AudioClip</param>
+    void RegisterClip(AudioClip clip);
+}
---
+++ Assets/Scripts/05. EffectManager/IEffectManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public interface IEffectManager
+{
+    /// <summary>
+    /// 특정 효과 재생 (예: 파티클, 프리팹, 애니메이션)
+    /// </summary>
+    void PlayEffect(string effectName, UnityEngine.Vector3 position);
+    
+
+    /// <summary>
+    /// 특정 효과를 일정 시간 후 제거
+    /// </summary>
+    void StopEffect(string effectName);
+
+
+    /// <summary>
+    /// 모든 활성 효과 제거
+    /// </summary>
+    void ClearAllEffects();
+}
