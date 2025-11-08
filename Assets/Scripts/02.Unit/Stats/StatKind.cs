9d31c39d484b7464de13c00054ffad1b4507c854
TW-K <ktwqwe12@daum.net>
2025-11-08 15:37:15 +0900

    아이템 자로구조 구현 중
    - 아이템 속성 적용, 생성 로직 구현하면 됨

--- Assets/Scripts/02.Unit/Stats/StatKind.cs
+++ Assets/Scripts/02.Unit/Stats/StatKind.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public enum StatKind
 {
-    MaxHealth,
-    Attack,
+    MaxHealth,              // HP 최대치
+    HealthRegenPerSec,      // 초당 HP 재생(옵션)
+    Attack,                 // 공격력
     Defense,                // 기존 방어력(깡 감소)
     DamageReducePct,        // 피해감소 % (0~1)
-    MoveSpeed,
+    MoveSpeed,              // 이동속도
     AttackSpeed,            // 공격속도(초당 공격)
     CritChance,             // 0~1
     CritMultiplier,         // ex) 1.5
@@ -14,5 +15,6 @@ public enum StatKind
     ManaRegenPerSec,        // 초당 MP 재생(옵션)
     CooldownReducePct,      // 쿨감 % (0~1)
     StatusPower,            // 상태이상 강도(스턴/슬로우 등
+    AggroValue,            // 어그로 수치
 }
 
