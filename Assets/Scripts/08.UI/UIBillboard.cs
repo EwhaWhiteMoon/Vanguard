c2ff6e181728e2cf635ac7d60d72b5d4112833dd
YunGuHan <hanyun9@naver.com>
2025-11-19 22:10:40 +0900

    main image추가 main화면 메뉴 버튼 수정 HP,MP 바 추가

---
+++ Assets/Scripts/08.UI/UIBillboard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class UIBillboard : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        if (cam == null) return;
+
+        // UI가 카메라를 바라보게 만들기
+        transform.LookAt(transform.position + cam.transform.forward);
+    }
+}
